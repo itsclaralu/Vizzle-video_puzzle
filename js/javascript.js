2bdab6708d0bbfb049db4ad7fcddeae302733ad7
claralm <clara.luismingueza@gmail.com>
2016-03-18 01:54:33 +0100

    Play button & Timer

--- js/javascript.js
+++ js/javascript.js
@@ -1,43 +1,51 @@
+/* Variables: */
+var clickCounter = 0; //Will be != 1 if we've already launched the game
+var start;
+var timer;
+
 /* Initial set ups*/
-document.ready = function() {
-  button = addEventListener('click', switchButton);
+window.onload = function() {
+  button = document.getElementById("play-button");
+  button.addEventListener('click', switchButton);
+}
+
+function counter() {
+  var now = new Date(); //Variable that stores the present's date
+  /*Time spent is the difference between now and start*/
+  var displayedSec = now.getSeconds() - start.getSeconds();
+  console.log("Sec: " + displayedSec);
+  var displayedMin = now.getMinutes() - start.getMinutes();
+  console.log("Min: " + displayedMin);
+  var displayedHours = now.getHours() - start.getHours();
+  console.log("H: " + displayedHours);
+  var displayedDate =  new Date(0, 0, 0, displayedHours, displayedMin, displayedSec);
+      
+  /*Showing the result*/
+  console.log(displayedDate);
+  document.getElementById("game-counter").innerHTML = displayedDate.toLocaleTimeString();
+  console.log(displayedDate);
 }
 
 /* Switching the button to play/pause*/
-button = document.getElementbyId("play-button");
-var i = 0; //Will be != 0 if we've already launched the game
-
 function switchButton() {
   if(button.id == "play-button") {
+      clickCounter++;
       button.setAttribute("src", "images/vizzle_pause.svg");
       button.setAttribute("id", "pause-button");
-      if( i == 0) { // Launch the game
+      if( clickCounter == 1) { // Launch the game
            //TODO: Aquí hacemos que comience el juego
+          start = new Date(); //Variable that stores the date when play button is pushed
+          counter();
+          timer = setInterval(counter, 1000); //We need to execute counter() each second (1000 miliseconds)
       }else { // Resume the game
           //TODO: Aquí hacemos que se reanude el juego
+          timer = setInterval(counter, 1000);
       }
-  }else {
+  }else { // Game paused
+      clickCounter++;
       button.setAttribute("src", "images/vizzle_play.svg");
       button.setAttribute("id", "play-button");
       //TODO: Aquí hacemos que se pause el juego
+      clearInterval(timer);
   }
-}
-
-function timing() {
-    var start = new Date.now(); //Variable that stores the date when play button is pushed
-      function counter() {
-      var now = new Date.now(); //Variable that stores the present's date
-      /*Time spent is the difference between now and start*/
-      var displayedSec = now.getSeconds() - start.getSeconds();
-      var displayedMin = now.getMinutes() - start.getMinutes();
-      var displayedHours = now.getHours() - start.getHours();
-      
-      var displayedDate =  new Date(displayedHours, displayedMin, displayedSec);
-      
-      /*Showing the result*/
-      document.getElementbyId(game-counter).innerHTML = displayedDate.toLocateString();
-      }
-    
-    counter();
-    var aux = setInterval(counter, 1000); //We need to execute counter() each second (1000 miliseconds)
 }
